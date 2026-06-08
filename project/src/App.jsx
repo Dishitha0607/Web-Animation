4befec3a860a39ef695db12f10e148583b415dcd
Dishitha0607 <dishi0607@gmail.com>
2026-06-08 14:54:34 +0530

    about section

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,7 +13,8 @@ function App() {
       <main>
         <Navbar />
         <Hero />
-        <Cocktails/>
+        <Cocktails />
+        <About />
       </main>
     </>
   );
