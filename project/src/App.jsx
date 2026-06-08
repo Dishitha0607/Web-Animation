57aac47cc139b3df5e2dc61242613db025242c46
Dishitha0607 <dishi0607@gmail.com>
2026-06-08 10:23:20 +0530

    added css

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -6,8 +6,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
     <>
-      <div>
-        <h2 className="text-3xl text-indigo-300">Hello! GSAP</h2>
+      <div className="flex-center h-[100vh]">
+        <h1 className="text-3xl text-indigo-300">Hello! GSAP</h1>
       </div>
     </>
   );
